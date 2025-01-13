9d8f3eb87cd6069f96ad8d8f2eab064ae77946bf
prathamesh86 <prathamesh.86agency@gmail.com>
2025-01-13 18:00:13 +0530

    fix : added big texts

--- react-lanuage-translator/src/App.js
+++ react-lanuage-translator/src/App.js
@@ -6,13 +6,20 @@ import { useTranslation } from "react-i18next";
 function App() {
   const locales = {
     en: { title: "English" },
-    de: { title: "german" },
-    fr: { title: "french" },
-    es: { title: "Español" },
+    de: { title: "German" },
+    fr: { title: "French" },
+    es: { title: "Spanish" },
+    hi_IN: { title: "Hindi" },
   };
 
   const { t, i18n } = useTranslation();
-  const [messages, setMessages] = useState(0);
+  const [selectedLocale, setSelectedLocale] = useState(i18n.resolvedLanguage);
+
+  const handleLanguageChange = (locale) => {
+    i18n.changeLanguage(locale);
+    setSelectedLocale(locale);
+  };
+
   return (
     <div>
       <ul>
@@ -20,11 +27,10 @@ function App() {
           <li key={locale}>
             <button
               style={{
-                fontWeight:
-                  i18n.resolvedLanguage === locale ? "bold" : "normal",
+                fontWeight: selectedLocale === locale ? "bold" : "normal",
               }}
-              type="submit"
-              onClick={() => i18n.changeLanguage(locale)}
+              type="button"
+              onClick={() => handleLanguageChange(locale)}
             >
               {locales[locale].title}
             </button>
@@ -32,7 +38,29 @@ function App() {
         ))}
       </ul>
       <h1>{t("main.header")}</h1>
-      <h3>{t("main.description")}</h3>
+      <p>{t("main.description")}</p>
+
+      <h3>Select Options:</h3>
+      <ul>
+        {Object.keys(locales).map((locale) => (
+          <li key={locale}>
+            <strong>{t(`main.select.options.${locale}.value`)}</strong> -{" "}
+            {t(`main.select.options.${locale}.description`)}
+          </li>
+        ))}
+      </ul>
+
+      <h3>Informative Paragraphs:</h3>
+      <div>
+        {t("main.informative.paragraphs", { returnObjects: true }).map(
+          (paragraph, index) => (
+            <div key={index} style={{ marginBottom: "1rem" }}>
+              <h4>{paragraph.title}</h4>
+              <p>{paragraph.content}</p>
+            </div>
+          )
+        )}
+      </div>
     </div>
   );
 }
